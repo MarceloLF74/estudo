37196e0a5169c14acc5f95653e0b859566e80da9
Marcelolf74 <marceloael@gmail.com>
2025-01-20 12:01:40 -0300

    subir aula 18

--- aula_18/exerc_01.py
+++ aula_18/exerc_01.py
@@ -1,20 +1,18 @@
 class ContaBancaria:
-    def __init__(self, saldo):
-        self.__saldo = saldo
+    def __init__(self, saldo: float):
+        self.__saldo = 0
 
-    def depositar(self, valor):
-        self.depositar += valor
+    def depositar(self, valor: float):
+        self.__saldo += valor
 
-    def sacar(self, valor):
-        if __saldo > 0:
+    def sacar(self, valor: float):
+        if __saldo >= 0:
             self.sacar -= valor
         else:
             print('Saldo insuficiente')
 
-    def verificar_saldo(self, saldo):
+    def verificar_saldo(self):
         self.__verificar_saldo = saldo
 
-
-saldo = 0
-
-
+conta1 = ContaBancaria()
+conta1.__saldo = 100000
